f85f0e1cdb621921ab9fc80bd73d66f9f9896a62
Lakshay Kaushik <lakshay.kaushik@lakshay.kaushik.successivetech.com>
2021-01-18 09:36:52 +0530

    Day 2

---
+++ src/modules/index.js
@@ -0,0 +1,17 @@
+import pkg from 'merge-graphql-schemas';
+const { fileLoader, mergeTypes } = pkg;
+import path from 'path';
+import * as User from './user/index.js';
+
+const dirname = path.resolve();
+const typeArray = fileLoader(path.join(dirname, './**/*.graphql'));
+const typeDefs = mergeTypes(typeArray, { all: true });
+
+export default {
+    resolvers: {
+        Query: {
+            ...User.getMyProfile,
+        },
+    },
+    typeDefs,
+}; 
