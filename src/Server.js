ba041c6b13b07a07eb7a030181a4010f03024d5d
Lakshay Kaushik <lakshay.kaushik@lakshay.kaushik.successivetech.com>
2021-01-20 09:30:51 +0530

    Day 2

--- src/Server.js
+++ src/Server.js
@@ -26,12 +26,12 @@ return this;
 }
 
 run() {
-const { app } = this;
-app.listen(9001, (err) => {
+const { app,config: { PORT }} = this;
+app.listen(PORT, (err) => {
 if (err) {
 console.log(err);
 }
-console.log(`App is running on port ${9001}`);
+console.log(`App is running on port ${PORT}`);
 });
 return this;
 }
